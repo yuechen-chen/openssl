d8086e38551a31c2da5e01f66d076414c54a5791
yuechen-chen <yuechen.chen@oracle.com>
2021-05-13 04:48:10 -0700

    Demo for ecdsa test. Add comments.

--- demos/ecdsa/EVP_ECDSA_demo.c
+++ demos/ecdsa/EVP_ECDSA_demo.c
@@ -1,3 +1,17 @@
+/*-
+ * Copyright 2021 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+/*
+ * Example of using EVP_PKEY, EVP_MD* and EVP_Digest* methods to calculate
+ * a signature using ecdsa
+ */
+
 #include <string.h>
 #include <stdio.h>
 #include <openssl/opensslconf.h>
@@ -21,6 +35,10 @@ static int ecdsa_create_pkey(EVP_PKEY **pkey, const char *curve_name,
                              int expected)
 {
     int ret = 0;
+    /* The EVP_PKEY_CTX structure is an opaque public key algorithm context
+     * used by the OpenSSL high-level public key API. 
+     * See openssl/doc/man3/EVP_PKEY_CTX_new.pod for more details.
+     */
     EVP_PKEY_CTX *ctx = NULL;
     OSSL_PARAM_BLD *bld = NULL;
     OSSL_PARAM *params = NULL;
@@ -31,6 +49,10 @@ static int ecdsa_create_pkey(EVP_PKEY **pkey, const char *curve_name,
     OSSL_PARAM_BLD_push_octet_string(bld, OSSL_PKEY_PARAM_PUB_KEY,
                                      pub, pub_len);
     params = OSSL_PARAM_BLD_to_param(bld);
+    
+    /* Allocates a public key algorithm context using the library context
+     * libctx. See openssl/doc/man3/EVP_PKEY_CTX_new.pod for more details.
+     */
     ctx = EVP_PKEY_CTX_new_from_name(libctx, "EC", NULL);
     EVP_PKEY_fromdata_init(ctx);
     EVP_PKEY_fromdata(ctx, pkey, EVP_PKEY_PUBLIC_KEY, params);
@@ -49,14 +71,17 @@ int ecdsa_demo(int id)
     int ret = 0;
     EVP_MD_CTX *md_ctx = NULL;
     EVP_PKEY *pkey = NULL;
+    /* ECDSA_SIG is an opaque structure consisting of two BIGNUMs for the r and s 
+     * value of an ECDSA signature. See  openssl/doc/man3/ECDSA_SIG_new.pod for
+     * more details
+     */
     ECDSA_SIG *sign = NULL;
     size_t sig_len;
     unsigned char *sig = NULL;
     BIGNUM *rbn = NULL, *sbn = NULL;
     const struct ecdsa_sigver_st *tst = &ecdsa_sigver_data[id];
-
+    
     ecdsa_create_pkey(&pkey, tst->curve_name, tst->pub, tst->pub_len, 1);
-
 
     sign = ECDSA_SIG_new();
     rbn = BN_bin2bn(tst->r, tst->r_len, NULL);
